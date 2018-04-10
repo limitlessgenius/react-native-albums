17eef6358fcfd7ecd4df895c970e43860a26f536
José Manuel <expressmillionaire@hotmail.com>
2018-04-11 00:33:51 +0200

    header progress

---
+++ index.ios.js
@@ -0,0 +1,27 @@
+
+// // Import libraries 
+// import React from 'react'
+// import { Text, AppRegistry } from 'react-native'
+
+// // Create a component
+
+// const App = () => {
+// 	return(
+// 		<Text> React Native App </Text>
+// 	)
+// }
+
+// //Implicit return if only return JSX	
+// AppRegistry.registerComponent('ReactNativeAlbums', () => App)
+// // () => implicit return App
+
+//Where does index.ios.js come from in the tutorial and what is its utility? 
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 
+// Import libraries 
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import Header from './src/components/component_header'
+
+// Create a component
+
+const App = () => (
+
+	<Header headerText={'Albums'}/>
+)
+
+
+//Implicit return if only return JSX	
+AppRegistry.registerComponent('ReactNativeAlbums', () => App)
+// () => implicit return App
+
+
+
+
+
+
+
+
+
+
+
+
