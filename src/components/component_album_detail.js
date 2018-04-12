a6b9a18c0ae68a12a071d7ba60176f1425a4d1c2
José Manuel <expressmillionaire@hotmail.com>
2018-04-12 22:24:35 +0200

    card section component

--- src/components/component_album_detail.js
+++ src/components/component_album_detail.js
@@ -4,16 +4,15 @@ import React from 'react'
 import { View, Text, Button, Image } from 'react-native'
 
 import Card from './component_card'
+import CardSection from './component_card_section'
 
 const AlbumDetail = (props) => {
 
-	console.log(props.album.title)
-
 	return (
 		<Card>
-
-			<Text>{props.album.title}</Text>
-			
+			<CardSection>
+				<Text>{props.album.title}</Text>
+			</CardSection>
 		</Card>
 	)
 }
